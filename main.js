111b49e52b2668421fc8ddecb8df8b1bc1248843
Eric Dawson <edawson42@gmail.com>
2018-10-11 13:24:22 -0700

    Update interface on navbar to glowing active choice

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let menu = document.getElementById("menu");
 
 menu.addEventListener("click", function() {
+    this.blur();
     document.getElementById("menulist").classList.toggle("active");
 });
